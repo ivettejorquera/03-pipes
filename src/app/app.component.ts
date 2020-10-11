8904307feca07e2c572ebc0a28c4249091fa7bf0
Ivette Jorquera <ivette.jorquera.buzeta@gmail.com>
2020-10-11 13:58:03 -0300

    app-pipes
    nueva app pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes';
+
+  nombre: string = 'Capitán América';
+  nombre2: string = 'IveTtE joRQuErA BuZEtA';
+
+  arreglo = [1, 2 , 3 , 4 , 5 , 6 , 7 , 8 , 9 , 10];
+
+  PI: number = Math.PI;
+  porcentaje: number = 0.234;
+  salario: number = 1234.5;
+  fecha: Date = new Date();
+  activar: boolean = true;
+
+  idioma: string = 'es';
+  videoUrl: string = 'https://www.youtube.com/embed/fjQv1J9yvyE';
+
+  valorPromesa = new Promise<string>( (resolve) => {
+      setTimeout(() => {
+        resolve('Llego la data');
+      }, 4500);
+  });
+
+  heroe = {
+    nombre: 'Logan',
+    clave: 'Wolverine',
+    edad: 500,
+    direccion: {
+      calle: 'Primera',
+      casa: 20
+    }
+  }
+
+ 
 }
